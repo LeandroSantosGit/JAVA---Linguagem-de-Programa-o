269bbfd9fcef04db5b308c10fc643d17c8ee7418
Leandro <rockgolmetal21@gmail.com>
2019-09-05 19:20:03 -0400

    Atualização

---
+++ Aulas/src/br/com/faculdade/aula01/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+
+package br.com.faculdade.aula01;
+
+/**
+ *
+ * @author Leandro
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Bem Vindo ao Mundo Java");
+    }
+    
+}
