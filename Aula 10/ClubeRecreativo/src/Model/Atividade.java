6e782f0ceb3ecb62755aa628db5b6cbd42e56e90
Leandro Henrique Pereira dos Santos <rockgolmetal21@gmail.com>
2019-12-01 18:59:25 -0400

    Aula 10

---
+++ Aula 10/ClubeRecreativo/src/Model/Atividade.java
@@ -0,0 +1,31 @@
+package Model;
+
+/**
+ * Date 30/11/2019
+ * @author Leandro
+ */
+public class Atividade {
+
+    private int codAtv;
+    private String descricao;
+
+    public Atividade() {
+
+    }
+
+    public int getCodAtv() {
+        return codAtv;
+    }
+
+    public void setCodAtv(int codAtv) {
+        this.codAtv = codAtv;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
